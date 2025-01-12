db4a85550a35efc488ad56e4b72a4ccaa646ebfc
anatfernandes <analuizateixeirafernandes184@gmail.com>
2025-01-12 17:13:45 -0300

    ♻️ | refac: refactor bird

--- src/bird.py
+++ src/bird.py
@@ -14,14 +14,17 @@ class Bird:
     MAXIMUM_ROTATION = 25
     ROTATION_SPEED = 20
     TIME_ANIMATION = 5
+    ACELERATION = 3
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
         self.time = 0
         self.angle = 0
         self.speed = 0
         self.image_count = 0
+
         self.height = self.y
         self.image = self.IMAGES[0]
 
@@ -31,11 +34,11 @@ class Bird:
         self.height = self.y
 
     def move(self):
-        # calcula o deslocamento
         self.time += 1
-        shift = 1.5 * (self.time**2) + self.speed * self.time
 
-        # restringe o deslocamento
+        # ((1/2) * ​a * t²) + (v0​ * t)
+        shift = 0.5 * self.ACELERATION * (self.time**2) + self.speed * self.time
+
         if shift > 16:
             shift = 16
         elif shift < 0:
@@ -43,16 +46,35 @@ class Bird:
 
         self.y += shift
 
-        # ângulo do pássaro
-        if shift < 0 or self.y < (self.height + 50):
-            if self.angle < self.MAXIMUM_ROTATION:
-                self.angle = self.MAXIMUM_ROTATION
+        self.set_angle(shift)
+
+    def draw(self, screen):
+        self.set_image()
+
+        rotated_image = pygame.transform.rotate(self.image, self.angle)
+        position_center_imagem = self.image.get_rect(topleft=(self.x, self.y)).center
+        rectangle = rotated_image.get_rect(center=position_center_imagem)
+
+        screen.blit(rotated_image, rectangle.topleft)
+
+    def set_angle(self, shift):
+        is_negative_shift = shift < 0
+        is_valid_position = self.y < (self.height + 50)
+        is_valid_angle = self.angle < self.MAXIMUM_ROTATION
+
+        if (is_negative_shift or is_valid_position) and is_valid_angle:
+            self.angle = self.MAXIMUM_ROTATION
+
         elif self.angle > -90:
             self.angle -= self.ROTATION_SPEED
 
-    def draw(self, screen):
-        # define qual imagem do pássaro vai usar
+    def set_image(self):
         self.image_count += 1
+
+        if self.angle <= -80:
+            self.image = self.IMAGES[1]
+            self.image_count = self.TIME_ANIMATION * 2
+            return
 
         if self.image_count < self.TIME_ANIMATION:
             self.image = self.IMAGES[0]
@@ -65,17 +87,6 @@ class Bird:
         elif self.image_count < self.TIME_ANIMATION * 4 + 1:
             self.image = self.IMAGES[0]
             self.image_count = 0
-
-        # se o pássaro tiver caindo não bate asas
-        if self.angle <= -80:
-            self.image = self.IMAGES[1]
-            self.image_count = self.TIME_ANIMATION * 2
-
-        # desenha a imagem
-        rotated_image = pygame.transform.rotate(self.image, self.angle)
-        position_center_imagem = self.image.get_rect(topleft=(self.x, self.y)).center
-        rectangle = rotated_image.get_rect(center=position_center_imagem)
-        screen.blit(rotated_image, rectangle.topleft)
 
     def get_mask(self):
         return pygame.mask.from_surface(self.image)
