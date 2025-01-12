3b06cd040fb31db04a133ce39ec3fedceda3ee94
anatfernandes <analuizateixeirafernandes184@gmail.com>
2025-01-12 17:12:17 -0300

    ♻️ | refac: rename variable name at get_imagem util

--- src/utils/get_image.py
+++ src/utils/get_image.py
@@ -3,7 +3,7 @@ import pygame
 
 
 def get_image(image):
-    image_name = f"{image}.png"
-    pygame_image = pygame.image.load(os.path.join("images", image_name))
+    file_name = f"{image}.png"
+    pygame_image = pygame.image.load(os.path.join("images", file_name))
 
     return pygame.transform.scale2x(pygame_image)
